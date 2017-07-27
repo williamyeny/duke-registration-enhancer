6b7072c0b1fb1d03660550e8c5537b715ed70921
will ye <williamyeny@gmail.com>
2017-07-27 19:32:36 -0400

    instant removal of courses section

--- js/main.js
+++ js/main.js
@@ -1,13 +1,59 @@
 console.log("enhancer loaded");
 
-
-
+//get extension css url
 var cssUrl = chrome.runtime.getURL("css/restyle.css");
 console.log("css url: " + cssUrl);
 
 $("iframe").on("load", function() {
+  //inject css into iframe
   $(this).contents().find("head").append("<link rel='stylesheet' href='" + cssUrl + "'>'");
+
+
+
 });
+
+//Runs when the main registration window loads
+$("#ptifrmtgtframe").on("load", function() {
+  var observer = new MutationObserver(function(mutations) {
+    console.log("changed");
+    /*
+    We need to add the click listener with every iframe DOM change because the
+    change removes the listener
+    */
+    dropDownAddClickListener();
+
+  });
+  observer.observe($(this).contents().find("body")[0], {childList: true, subtree: true});
+});
+
+
+//Runs when the inner dropdown buttons are clicked
+function dropDownAddClickListener() {
+  $("#ptifrmtgtframe").contents().find("a[id*='DU_SEARCH_WRK_SSR_EXPAND_COLLAPS']").on("click", function(e) {
+    //if it is in expanded form
+    var buttonImage = $(this).children();
+    if (buttonImage.attr("src") == "/cs/CSPRD01/cache/PS_COLLAPSE_ICN_1.gif") {
+      console.log("courses section is expanded");
+      e.preventDefault();
+      var buttonId = $(this).attr("id");
+      var buttonNumber = buttonId[buttonId.length - 1];
+
+      /*
+      delete the expanded courses section!
+      (we are forced to use .parent() is because the tr's dont have ids!)
+      */
+      var coursesSection = $("#ptifrmtgtframe").contents().find("#win0divGROUP_CAT\\$" + buttonNumber).parent().parent();
+      //remove the spacing tr first
+      coursesSection.next().remove();
+      //remove the actual tr
+      coursesSection.remove();
+
+      //restore "expanded" button image
+      buttonImage.attr("src", "/cs/CSPRD01/cache/PS_EXPAND_ICN_1.gif");
+    }
+  });
+}
+
 
 // var observer = new MutationObserver(function(mutations) {
 //

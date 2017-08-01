26336c02eb8bb844b4993f3da6a0a0a14cfb84dd
will ye <williamyeny@gmail.com>
2017-07-31 23:44:22 -0400

    added options page

---
+++ js/options.js
@@ -0,0 +1,32 @@
+// Saves options to chrome.storage.sync.
+function save_options() {
+  var appearanceUpgrades = document.getElementById('appearance-upgrades').checked;
+  var instantCollapse = document.getElementById('instant-collapse').checked;
+  chrome.storage.sync.set({
+    appearanceUpgrades: appearanceUpgrades,
+    instantCollapse: instantCollapse
+  }, function() {
+    // Update status to let user know options were saved.
+    var status = document.getElementById('status');
+    status.textContent = 'Options saved.';
+    setTimeout(function() {
+      status.textContent = '';
+    }, 750);
+  });
+}
+
+// Restores select box and checkbox state using the preferences
+// stored in chrome.storage.
+function restore_options() {
+  chrome.storage.sync.get({
+    appearanceUpgrades: true, //default values
+    instantCollapse: false
+  }, function(items) {
+    document.getElementById('appearance-upgrades').checked = items.appearanceUpgrades;
+    document.getElementById('instant-collapse').checked = items.instantCollapse;
+  });
+}
+
+document.addEventListener('DOMContentLoaded', restore_options);
+document.getElementById('save').addEventListener('click',
+    save_options);

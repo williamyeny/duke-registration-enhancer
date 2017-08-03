c1afecf96bf70219f318df6b200494611b552040
williamyeny <williamyeny@gmail.com>
2017-08-02 23:15:47 -0400

    code organization

--- extension/js/injectJs.js
+++ extension/js/injectJs.js
@@ -2,17 +2,23 @@ function injectJs(options) {
     //Runs when the main registration window loads
     $("#ptifrmtgtframe").on("load", function() {
 
-        var defaultIframeDocument = $("#DU_SEARCH_SUBJECT");
-        console.log("default: " + defaultIframeDocument);
         var observer = new MutationObserver(function(mutations) {
             console.log("changed");
             /*
             We need to add the click listener with every iframe DOM change because the
             change removes the listener
             */
-            if (options.instantCollapse) dropDownAddClickListener(defaultIframeDocument);
+            dropDownAddClickListener(options);
 
         });
         observer.observe($(this).contents().find("body")[0], {childList: true, subtree: true});
     });
+}
+
+//Runs when the inner dropdown buttons are clicked
+function dropDownAddClickListener(options) {
+  var iframeContents = $("#ptifrmtgtframe").contents();
+  iframeContents.find("a[id*='DU_SEARCH_WRK_SSR_EXPAND_COLLAPS']").on("click", function(e) {
+    if (options.instantCollapse) instantCollapse(this, iframeContents, e);
+  });
 }
--- extension/js/instantCollapse.js
+++ extension/js/instantCollapse.js
@@ -1,35 +1,32 @@
-//Runs when the inner dropdown buttons are clicked
-function dropDownAddClickListener(defaultIframeDocument) {
-  var iframeContents = $("#ptifrmtgtframe").contents();
-  iframeContents.find("a[id*='DU_SEARCH_WRK_SSR_EXPAND_COLLAPS']").on("click", function(e) {
+function instantCollapse(button, iframeContents, e) {
     //if it is in expanded form
-    var buttonImage = $(this).children();
-    if (buttonImage.attr("src") == "/cs/CSPRD01/cache/PS_COLLAPSE_ICN_1.gif") {
-      console.log("courses section is expanded");
-      e.preventDefault();
+  var buttonImage = $(button).children();
+  if (buttonImage.attr("src") == "/cs/CSPRD01/cache/PS_COLLAPSE_ICN_1.gif") {
+    console.log("courses section is expanded");
+    e.preventDefault();
 
-      var buttonId = $(this).attr("id");
-      //just get number off of the end of the ID
-      var buttonNumber = buttonId.replace("DU_SEARCH_WRK_SSR_EXPAND_COLLAPS$", "");
+    var buttonId = $(button).attr("id");
+    //just get number off of the end of the ID
+    var buttonNumber = buttonId.replace("DU_SEARCH_WRK_SSR_EXPAND_COLLAPS$", "");
 
-      /*
-      delete the expanded courses section!
-      (we are forced to use .parent() is because the tr's dont have ids!)
-      */
-      var coursesSection = iframeContents.find("#win0divGROUP_CAT\\$" + buttonNumber).parent().parent();
-      //remove the spacing tr first
-      console.log("HTML: " + coursesSection.next().html());
-      coursesSection.next().remove();
-      //remove the actual tr
-      coursesSection.remove();
+    /*
+    delete the expanded courses section!
+    (we are forced to use .parent() is because the tr's dont have ids!)
+    */
+    var coursesSection = iframeContents.find("#win0divGROUP_CAT\\$" + buttonNumber).parent().parent();
+    //remove the spacing tr first
+    console.log("HTML: " + coursesSection.next().html());
+    coursesSection.next().remove();
+    //remove the actual tr
+    coursesSection.remove();
 
-      //restore "expanded" button image
-      buttonImage.attr("src", "/cs/CSPRD01/cache/PS_EXPAND_ICN_1.gif");
+    //restore "expanded" button image
+    buttonImage.attr("src", "/cs/CSPRD01/cache/PS_EXPAND_ICN_1.gif");
 
-      iframeContents.find("#ICStateNum").val(parseInt(iframeContents.find("#ICStateNum").val()) + 1);
-    } else { //if it's not in expanded form
-      defaultIframeDocument = iframeContents.find("#DU_SEARCH_SUBJECT")[0];
-      console.log("default: " + defaultIframeDocument);
-    }
-  });
+    //TODO: trick the site into thinking it is truly collapsed so attempting to reopen a collapsed subject won't reset it
+    
+    // iframeContents.find("#ICStateNum").val(parseInt(iframeContents.find("#ICStateNum").val()) + 1);
+  } else { //if it's not in expanded form
+
+  }
 }
